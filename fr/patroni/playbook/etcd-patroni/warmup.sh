a20272546d8c55fa97f4332498ad334ca2e3b7cb
Franck Boudehen <franck.boudehen@dalibo.com>
2023-02-07 17:02:32 +0100

    resync avec le playbook de hapat

--- fr/patroni/playbook/etcd-patroni/warmup.sh
+++ fr/patroni/playbook/etcd-patroni/warmup.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Ce script permet de récupérer l'image Debian de base pour
+# accélérer la création des conteneurs LXC suivants.
+
 set -x
 
 sudo lxc-create -n warmup -t debian -- -r buster
